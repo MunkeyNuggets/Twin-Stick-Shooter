851b59a5a48552861ae1cecfb15323434fc75c69
Ben Houghton <Ben Houghton>
2016-09-28 19:13:12 +1000

    First upload. Currently broken.

---
+++ Twin Stick Shooter/Assets/Scripts/ShootOnAxisInput.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ShootOnAxisInput : MonoBehaviour {
+
+    public GameObject bullet;
+
+    public string horizontalAxis = "Horizontal";
+    public string verticalAxis = "Vertical";
+
+    public float shootDelay = 15f;
+
+    private bool canShoot = true;
+
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        Vector3 shootDirection = Vector3.right * Input.GetAxis(horizontalAxis) + Vector3.forward * Input.GetAxis(verticalAxis);
+        {
+            transform.rotation = Quaternion.LookRotation(shootDirection, Vector3.up);
+            Instantiate(bullet, transform.position, transform.rotation);
+
+            canShoot = false;
+            Invoke("ResetShot", shootDelay);
+        }
+
+    }
+
+    void resetShot()
+    {
+        canShoot = true;
+    }
+}
